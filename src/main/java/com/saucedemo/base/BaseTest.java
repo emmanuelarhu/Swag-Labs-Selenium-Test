608dc6b4407769429d0671655f427c5508d0f6e0
Emmanuel Arhu <emmanuelarhu706@gmail.com>
2025-07-23 21:43:29 +0000

    feat: update BaseTest for WebDriver setup and teardown

--- src/main/java/com/saucedemo/base/BaseTest.java
+++ src/main/java/com/saucedemo/base/BaseTest.java
@@ -15,8 +15,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 import java.time.Duration;
 
