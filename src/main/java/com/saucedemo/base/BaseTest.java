d60c54ce1a936f05c5775f1b9cd1bc9b8619568b
Emmanuel Arhu <emmanuelarhu706@gmail.com>
2025-07-23 11:09:21 +0000

    feat: add BaseTest for WebDriver setup and teardown

---
+++ src/main/java/com/saucedemo/base/BaseTest.java
@@ -0,0 +1,91 @@
+package com.saucedemo.base;
+
+import com.saucedemo.utils.ConfigReader;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.Attachment;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Optional;
+
+import java.time.Duration;
+
+public class BaseTest {
+    private static final Logger logger = LoggerFactory.getLogger(BaseTest.class);
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    private static final int TIMEOUT = 10;
+
+    @BeforeMethod
+    @Parameters({"browser"})
+    public void setUp(@Optional("chrome") String browser) {
+        logger.info("Setting up test environment for browser: {}", browser);
+        initializeDriver(browser);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(TIMEOUT));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(TIMEOUT));
+
+        String baseUrl = ConfigReader.getProperty("base.url");
+        logger.info("Navigating to: {}", baseUrl);
+        driver.get(baseUrl);
+    }
+
+    private void initializeDriver(String browser) {
+        switch (browser.toLowerCase()) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--disable-blink-features=AutomationControlled");
+                chromeOptions.addArguments("--disable-extensions");
+                chromeOptions.addArguments("--no-sandbox");
+                chromeOptions.addArguments("--disable-dev-shm-usage");
+                if (Boolean.parseBoolean(System.getProperty("headless", "false"))) {
+                    chromeOptions.addArguments("--headless");
+                }
+                driver = new ChromeDriver(chromeOptions);
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                if (Boolean.parseBoolean(System.getProperty("headless", "false"))) {
+                    firefoxOptions.addArguments("--headless");
+                }
+                driver = new FirefoxDriver(firefoxOptions);
+                break;
+            default:
+                throw new IllegalArgumentException("Browser not supported: " + browser);
+        }
+        logger.info("Driver initialized for browser: {}", browser);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            logger.info("Closing browser and cleaning up resources");
+            driver.quit();
+        }
+    }
+
+    @Attachment(value = "Screenshot", type = "image/png")
+    public byte[] takeScreenshot() {
+        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    public WebDriverWait getWait() {
+        return wait;
+    }
+}
